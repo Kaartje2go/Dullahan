cb0f3a02fd5c3cd2e8970ec14c214ed9a7814eb5
Sebastiaan Nijland <sebastiaan@kaartje2go.nl>
2020-08-18 16:09:18 +0200

    chore: use correct find conditions

--- packages/dullahan/src/api/DullahanApi.ts
+++ packages/dullahan/src/api/DullahanApi.ts
@@ -92,7 +92,7 @@ export class DullahanApi<
             await this.scrollToElement(selector, timeout);
         }
 
-        await adapter.waitForElementPresent(selector, {
+        await adapter.waitForElementVisible(selector, {
             timeout: timeout ?? defaultTimeout
         });
         await adapter.click(selector);
