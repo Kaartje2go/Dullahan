197d5263319afcf9a9ba109af4497106c80c12ff
Sebastiaan Nijland <sebastiaan@kaartje2go.nl>
2020-08-27 13:46:48 +0200

    chore: use force local flag

--- scripts/ci/setup/edge.sh
+++ scripts/ci/setup/edge.sh
@@ -1,2 +1,2 @@
 curl.exe -o "C:\SeleniumWebDrivers\EdgeDriver\edgedriver.zip" https://msedgedriver.azureedge.net/86.0.620.0/edgedriver_win64.zip
-tar -xf "C:\SeleniumWebDrivers\EdgeDriver\edgedriver.zip"
+tar --force-local -xf "C:\SeleniumWebDrivers\EdgeDriver\edgedriver.zip"
