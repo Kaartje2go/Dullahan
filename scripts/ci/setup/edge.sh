5af079f75d026932fc45cd7b75b561947f5ee627
Sebastiaan Nijland <sebastiaan@kaartje2go.nl>
2020-08-27 13:15:02 +0200

    chore: install edge in workflow

--- scripts/ci/setup/edge.sh
+++ scripts/ci/setup/edge.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
 set -e
+
+sudo apt-get install libgbm1
+
+wget -c -nc --retry-connrefused --tries=0 https://msedgedriver.azureedge.net/86.0.620.0/edgedriver_win64.zip
+unzip -o -q edgedriver_win64.zip
+sudo mv edgedriver C:\\SeleniumWebDrivers\\EdgeDriver
+rm edgedriver_win64.zip
