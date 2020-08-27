5102b5dd67af87823eaf1de1e722cc8bbaac61ab
Sebastiaan Nijland <sebastiaan@kaartje2go.nl>
2020-08-27 14:17:14 +0200

    fix: add timeout to make sure file is downloaded

--- scripts/ci/setup/edge.sh
+++ scripts/ci/setup/edge.sh
@@ -1,2 +1,3 @@
 curl.exe -o "C:\SeleniumWebDrivers\EdgeDriver\edgedriver.zip" https://msedgedriver.azureedge.net/86.0.620.0/edgedriver_win64.zip
-tar --force-local -xf "C:\SeleniumWebDrivers\EdgeDriver\edgedriver.zip"
+timeout 5
+tar.exe --force-local -xf "C:\SeleniumWebDrivers\EdgeDriver\edgedriver.zip"
