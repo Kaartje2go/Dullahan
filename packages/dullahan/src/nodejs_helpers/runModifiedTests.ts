86caa2dd55b894e378c1b4693444b707783ce320
Alfred Westerveld <alfredwesterveld@gmail.com>
2020-09-08 10:02:30 +0200

    AWS runner should also be able to only run modified tests (#90)

---
+++ packages/dullahan/src/nodejs_helpers/runModifiedTests.ts
@@ -0,0 +1,27 @@
+import {readFile,pathExists} from 'fs-extra';
+
+export const getChangedFiles = async () : Promise<string[]> => {
+    // file created in Dullahan-Tests repo with changed files from current branch using git history in drone build step
+    const file = './.changed-files.txt';
+    const fileExists = await pathExists(file);
+
+    if (fileExists) {
+        const data = await readFile(file, 'utf-8');
+        const splitted = data.split('\n');
+        // last element is empty line
+        splitted.splice(-1);
+        return splitted;
+    }
+    return [];
+}
+
+export const testIfOnlyTestsModified = (splited : string[]) : boolean => {
+    if (splited.length === 0) {
+        return false;
+    }
+    return splited.every(line => line.startsWith('tests/'));
+}
+
+export const testFile = (files: string[], fileToMatch: string) : boolean => {
+    return files.some(file => fileToMatch.endsWith(file));
+}
