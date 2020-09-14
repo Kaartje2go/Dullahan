45adec6ef68265b592a8790afb5410e73940b5e4
Sebastiaan Nijland <19267753+SebastiaanNijland@users.noreply.github.com>
2020-09-14 12:00:07 +0200

    chore: revert changes to edge script

--- scripts/ci/setup/edge.sh
+++ scripts/ci/setup/edge.sh
@@ -1,3 +1,3 @@
-curl.exe -o "C:\SeleniumWebDrivers\EdgeDriver\edgedriver.zip" https://msedgedriver.azureedge.net/86.0.620.0/edgedriver_win64.zip
-timeout 5
-tar.exe --force-local -xf "C:\SeleniumWebDrivers\EdgeDriver\edgedriver.zip"
+#!/usr/bin/env bash
+
+set -e
