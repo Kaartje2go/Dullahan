5717badd1460ecb42f76180e3782e57b1aba72f3
Alfred Westerveld <alfredwesterveld@gmail.com>
2020-10-07 12:29:17 +0200

    p-queue not use await (#134)
    * Try to use p-queue again but with higher second rate concurrency
    
    * Tue Oct  6 19:45:13 CEST 2020
    
    * Wed Oct  7 09:30:32 CEST 2020
    
    * Wed Oct  7 12:28:37 CEST 2020

--- packages/dullahan-runner-aws-lambda/src/DullahanRunnerAwsLambda.ts
+++ packages/dullahan-runner-aws-lambda/src/DullahanRunnerAwsLambda.ts
@@ -148,8 +148,8 @@ export default class DullahanRunnerAwsLambda extends DullahanRunner<
         );
         console.log(`Running tests with concurrency ${maxConcurrency}`);
 
-        const queue = new PQueue({ concurrency: maxConcurrency });
-        const retryQueue = new PQueue({ concurrency: 15, autoStart: false });
+        const queue = new PQueue({ concurrency: maxConcurrency, timeout: 5 * 60 * 1000 });
+        const retryQueue = new PQueue({ concurrency: 15, autoStart: false, timeout: 5 * 60 * 1000 });
 
         const addElement = async (testData) => {
             if (this.hasStopSignal) {
@@ -175,7 +175,7 @@ export default class DullahanRunnerAwsLambda extends DullahanRunner<
 
             if (hasMoreAttempts && couldStillPass) {
                 console.log("total failures", this.totalFailures++, testData);
-                await retryQueue.add(async () => await addElement(testData));
+                await retryQueue.add(async () => addElement(testData));
             } else if (failFast) {
                 this.hasStopSignal = true;
             }
@@ -183,13 +183,14 @@ export default class DullahanRunnerAwsLambda extends DullahanRunner<
 
         await Promise.all(  
             nextPool.map(async (testData) => {
-                await queue.add(async () => await addElement(testData));
+                await queue.add(async () => addElement(testData));
             })
         );
 
         console.log("wait for on idle main queue");
         await queue.onIdle();
         console.log("wait for on idle retry queue");
+        await sleep(5000);
         await retryQueue.start().onIdle();
         console.log("idle retry queue");
     }
